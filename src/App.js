91a17160d991305f8b3169797e8e9d711e9302d4
GayathriArundheepak <127670381+GayathriArundheepak@users.noreply.github.com>
2024-08-02 10:53:02 +0530

    todo list completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { TodoWrapper } from './components/TodoWrapper';
 function App() {
   return (
     <div className="App">
-    <h1>Todo List</h1>
+      <TodoWrapper  />
     </div>
   );
 }
